cf18d952ad42398536e4186bc75db93a5246a2c6
cris-null <cris@csalazar.net>
2021-03-24 10:03:20 -0500

    Generate random strings for authorization url state parameter

---
+++ src/main/kotlin/util/StringUtil.kt
@@ -0,0 +1,12 @@
+package util
+
+fun getRandomString(length: Int): String {
+    val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    return (1..length)
+        .map { allowedChars.random() }
+        .joinToString("")
+}
+
+fun getRandomString(): String {
+    return java.util.UUID.randomUUID().toString()
+}
