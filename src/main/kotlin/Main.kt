c014c4056da812b58b9119f16e55a07a94a819fc
cris-null <cris@csalazar.net>
2021-03-25 18:49:38 -0500

    Test access token retrieval

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import net.AccessTokenRetriever
 import util.authorization.AuthorizationUrlGenerator
 import util.string.RandomStringGenerator
 import util.string.RedirectUriParser
@@ -12,13 +13,22 @@ fun main() {
 
     val redirectUriResult = RedirectUriParser.getRedirectUriResult(redirectUri!!)
 
+    var code: String? = null
     when (redirectUriResult) {
         is RedirectUriResult.Success -> {
             if (redirectUriResult.state == state) println("state matches!")
             println("code = ${redirectUriResult.code}")
+            code = redirectUriResult.code
         }
         is RedirectUriResult.Error -> {
             println("error msg = ${redirectUriResult.message}")
         }
+    }
+
+    if (code != null) {
+
+        println("Enter you app's secret:")
+        val secret = readLine()
+        AccessTokenRetriever.getAuthToken(secret!!, code)
     }
 }
