0b71a225c0f06eb3d1402c57b7c22c112d8f5981
cris-null <cris@csalazar.net>
2021-03-26 16:03:37 -0500

    Improve documention of @Json annotation

--- src/main/kotlin/net/TokenResponseParser.kt
+++ src/main/kotlin/net/TokenResponseParser.kt
@@ -6,7 +6,7 @@ import com.beust.klaxon.Klaxon
 object TokenResponseParser {
 
     /**
-     * Parses a string of Reddit's response to a POST request for an access token and
+     * Parses a string of Reddit's response to a POST request, for an access token, and
      * turns it into a [RefreshTokenResponse].
      */
     fun parse(responseJson: String): RefreshTokenResponse? {
@@ -20,6 +20,10 @@ object TokenResponseParser {
  * Contains all the information from the JSON response for an access token as fields.
  */
 data class RefreshTokenResponse(
+    // The Json annotation for the Klaxon library allows to define what said variable
+    // will look for in the JSON file.
+    // Use mainly to maintain Kotlin style conventions of using camelCase, instead of
+    // temporarily using snake_case to properly parse the JSON file.
     @Json(name = "access_token")
     val accessToken: String,
     @Json(name = "token_type")
