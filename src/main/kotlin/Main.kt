e1281e6b1ba819a9d1335f3004e97e51daed2d90
cris-null <cris@csalazar.net>
2021-03-29 11:24:19 -0500

    Refactor main for clarity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,25 @@
 import authorization.AuthorizationManager
+import authorization.AuthorizationTokenPair
 import file.FileTokenManager
-import kotlinx.coroutines.GlobalScope
 
 fun main() {
-
-
 }
 
-fun printNewAccessTokens() {
-    val tokenPair = AuthorizationManager().getAuthorizationTokens()
-    println("""
-        Access token = ${tokenPair.accessToken.accessToken}
-        Seconds till expiration = ${tokenPair.accessToken.secondsTillExpiration()}
-        Refresh token = ${tokenPair.refreshToken}
-    """.trimIndent())
+fun getAuthTokenPair(): AuthorizationTokenPair {
+    return AuthorizationManager().getAuthorizationTokens()
+}
 
-    FileTokenManager.writeRefreshTokenToFile(tokenPair.refreshToken)
+fun printAuthTokensInfo(authorizationTokenPair: AuthorizationTokenPair) {
+    println(
+        """
+        Access token = ${authorizationTokenPair.accessToken.value}
+        Seconds till expiration = ${authorizationTokenPair.accessToken.secondsTillExpiration()}
+        Refresh token = ${authorizationTokenPair.refreshTokenValue}
+    """.trimIndent()
+    )
+}
+
+fun saveTokens(authorizationTokenPair: AuthorizationTokenPair) {
+    FileTokenManager.writeRefreshTokenToFile(authorizationTokenPair.refreshTokenValue)
     println(FileTokenManager.getRefreshTokenFromFile())
 }
