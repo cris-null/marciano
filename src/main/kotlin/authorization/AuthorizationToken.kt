43c9fa4927469057efb28c03d51c3e52f1886b34
cris-null <cris@csalazar.net>
2021-03-29 11:33:29 -0500

    Refactor AccessToken for clarity

--- src/main/kotlin/authorization/AuthorizationToken.kt
+++ src/main/kotlin/authorization/AuthorizationToken.kt
@@ -3,24 +3,26 @@ package authorization
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-data class TokenPair(val accessToken: AccessToken, val refreshToken: String)
+data class AuthorizationTokenPair(val accessToken: AccessToken, val refreshTokenValue: String)
 
 /**
  * @param duration How many Unix Epoch seconds will the token last. (default seems to be 1 hour, or 3600 seconds)
  */
-data class AccessToken(val accessToken: String, private val duration: Long) {
+data class AccessToken(val value: String, private val duration: Long) {
 
-    fun secondsTillExpiration(): Long {
-        // LocalDataTime to epoch seconds
+    /**
+     * Calculates how many seconds remain till this token expires, using Unix Epoch seconds.
+     */
+    fun getSecondsTillExpiration(): Long = getExpirationTime() - getCurrentTimeInEpochSeconds()
+
+    /**
+     * @return The moment when this token will expire, in Unix Epoch seconds.
+     */
+    fun getExpirationTime(): Long = getCurrentTimeInEpochSeconds() + duration
+    
+    private fun getCurrentTimeInEpochSeconds(): Long {
         val now = LocalDateTime.now(ZoneOffset.UTC)
         val currentTimeInEpochSeconds = now.atZone(ZoneOffset.UTC).toEpochSecond()
-        return determineExpirationTime() - currentTimeInEpochSeconds
-    }
-
-    private fun determineExpirationTime(): Long {
-        // LocalDataTime to epoch seconds
-        val now = LocalDateTime.now(ZoneOffset.UTC)
-        val currentTimeInEpochSeconds = now.atZone(ZoneOffset.UTC).toEpochSecond()
-        return currentTimeInEpochSeconds + duration
+        return currentTimeInEpochSeconds
     }
 }
