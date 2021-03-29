7e844ace26df8bfbc1b29bd242f7d2dd20ab7aaa
cris-null <cris@csalazar.net>
2021-03-29 11:03:25 -0500

    Refactor main for clarity

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 import authorization.AuthorizationManager
 import file.FileTokenManager
+import kotlinx.coroutines.GlobalScope
 
 fun main() {
+
+
+}
+
+fun printNewAccessTokens() {
     val tokenPair = AuthorizationManager().getAuthorizationTokens()
     println("""
         Access token = ${tokenPair.accessToken.accessToken}
