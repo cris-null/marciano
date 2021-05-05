a3e9a87f1a306bcdfdae0cae9823f240a8058c0c
cris-null <cris@csalazar.net>
2021-05-05 10:53:19 -0500

    Fetch next listing

--- src/main/kotlin/net/api/HistoryService.kt
+++ src/main/kotlin/net/api/HistoryService.kt
@@ -7,13 +7,23 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
+import java.security.cert.TrustAnchor
 
 interface HistoryService {
 
     @GET("/user/{username}/saved")
-    suspend fun getSaved(
+    suspend fun fetchSaved(
         @Header("Authorization") authorization: String,
         @Path("username") username: String,
+        @Header("User-Agent") userAgent: String = genUserAgent(),
+    ): Response<Listing>
+
+    @GET("/user/{username}/saved")
+    suspend fun fetchSaved(
+        @Header("Authorization") authorization: String,
+        @Path("username") username: String,
+        @Query("after") nextAnchor: String,
         @Header("User-Agent") userAgent: String = genUserAgent(),
     ): Response<Listing>
 }
